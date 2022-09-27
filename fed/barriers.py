d2702b3b833edb3c53d66dc67e5f41e157fe6241
Qing Wang <kingchin1218@gmail.com>
2022-09-27 09:54:49 +0800

    Use asyncio for grpc to avoid race condition. (#11)

--- fed/barriers.py
+++ fed/barriers.py
@@ -4,6 +4,7 @@ from concurrent import futures
 import cloudpickle
 import grpc
 import ray
+import asyncio
 
 from fed.grpc import fed_pb2, fed_pb2_grpc
 
@@ -11,48 +12,47 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
 class SendDataService(fed_pb2_grpc.GrpcServiceServicer):
-    def __init__(self, event, all_data):
-        self._event = event
+    def __init__(self, all_events, all_data):
+        self._events = all_events
         self._all_data = all_data
 
-    def SendData(self, request, context):
+    async def SendData(self, request, context):
         upstream_seq_id = int(request.upstream_seq_id)
         downstream_seq_id = int(request.downstream_seq_id)
         print(
             f"Received a grpc data from {upstream_seq_id} to {downstream_seq_id}"
         )
         self._all_data[downstream_seq_id] = request.data
-        self._event.set()
+        if upstream_seq_id not in self._events:
+            event = asyncio.Event()
+            self._events[upstream_seq_id] = event
+        event = self._events[upstream_seq_id]
+        event.set()
         print(f"=======Event set for {upstream_seq_id}")
         return fed_pb2.SendDataResponse(result="OK")
 
 
-def _run_grpc_server(port, event, all_data):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    fed_pb2_grpc.add_GrpcServiceServicer_to_server(
-        SendDataService(event, all_data), server
-    )
+async def _run_grpc_server(port, event, all_data):
+    server = grpc.aio.server()
+    fed_pb2_grpc.add_GrpcServiceServicer_to_server(SendDataService(event, all_data), server)
     server.add_insecure_port(f'[::]:{port}')
-    server.start()
+    await server.start()
     print("start service...")
-    try:
-        while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
-    except KeyboardInterrupt:
-        server.stop(0)
+    await server.wait_for_termination()
 
 
-def send_data_grpc(dest, data, upstream_seq_id, downstream_seq_id):
-    conn = grpc.insecure_channel(dest)
-    client = fed_pb2_grpc.GrpcServiceStub(channel=conn)
-    request = fed_pb2.SendDataRequest(
-        data=cloudpickle.dumps(data),
-        upstream_seq_id=str(upstream_seq_id),
-        downstream_seq_id=str(downstream_seq_id),
-    )
-    response = client.SendData(request)
-    print("received response:", response.result)
-    return response.result
+async def send_data_grpc(dest, data, upstream_seq_id, downstream_seq_id):
+    data = cloudpickle.dumps(data)
+    async with grpc.aio.insecure_channel(dest) as channel:
+        stub = fed_pb2_grpc.GrpcServiceStub(channel)
+        request = fed_pb2.SendDataRequest(
+            data=data,
+            upstream_seq_id=str(upstream_seq_id),
+            downstream_seq_id=str(downstream_seq_id),
+        )
+        response = await stub.SendData(request)
+        print("received response:", response.result)
+        return response.result
 
 
 def send_op(dest, data, upstream_seq_id, downstream_seq_id):
@@ -60,39 +60,39 @@ def send_op(dest, data, upstream_seq_id, downstream_seq_id):
     # if yes, we should send data, otherwise we should
     # send `ray.get(data)`
     print(f"Sending data to seq_id {downstream_seq_id} from {upstream_seq_id}")
-    response = send_data_grpc(dest, data, upstream_seq_id, downstream_seq_id)
+    response = asyncio.get_event_loop().run_until_complete(send_data_grpc(dest, data, upstream_seq_id, downstream_seq_id))
     print(f"Sent. response is {response}")
     return True  # True indicates it's sent successfully.
 
 
 @ray.remote
 class RecverProxyActor:
-    def __init__(self, listen_addr: str):
+    async def __init__(self, listen_addr: str):
         self._listen_addr = listen_addr
 
         # Workaround the threading coordinations
 
         # All events for grpc waitting usage.
-        import threading
-
-        self._event = threading.Event()
-        self._event.set()  # 设定Flag = True
+        self._events = {} # map from upstream_seq_id -> event
         self._all_data = {}
 
-    def run_grpc_server(self):
-        _run_grpc_server(
+    async def run_grpc_server(self):
+        return await _run_grpc_server(
             self._listen_addr[self._listen_addr.index(':') + 1 :],
-            self._event,
+            self._events,
             self._all_data,
         )
 
-    def is_ready(self):
+    async def is_ready(self):
         return True
 
-    def get_data(self, upstream_seq_id, curr_seq_id):
-        print(f"Getting data for {curr_seq_id} from {upstream_seq_id}")
-        self._event.clear()
-        self._event.wait()
+    async def get_data(self, upstream_seq_id, curr_seq_id):
+        print(f"====Getting data for {curr_seq_id} from {upstream_seq_id}")
+        if upstream_seq_id not in self._events:
+            self._events[upstream_seq_id] = asyncio.Event()
+        
+        curr_event = self._events[upstream_seq_id]
+        await curr_event.wait()
         print(f"=======Waited for {curr_seq_id}")
         data = self._all_data[curr_seq_id]
         return cloudpickle.loads(data)
---
+++ tests/test_transport_proxy.py
@@ -0,0 +1,38 @@
+import pytest 
+
+import ray
+
+from fed.barriers import RecverProxyActor, send_op
+
+
+def test_n_to_1_transport():
+    """This case is used to test that we have N send_op barriers,
+    sending data to the target recver proxy, and there also have
+    N receivers to `get_data` from Recver proxy at that time.
+    """
+    ray.init()
+    NUM_DATA = 10
+    SERVER_ADDRESS = "127.0.0.1:12344"
+    recver_proxy_actor = RecverProxyActor.options(
+        name=f"RecverProxyActor-TEST", max_concurrency=2000
+    ).remote(SERVER_ADDRESS)
+    recver_proxy_actor.run_grpc_server.remote()
+    assert ray.get(recver_proxy_actor.is_ready.remote())
+
+    sent_objs = []
+    get_objs = []
+    for i in range(NUM_DATA):
+        sent_obj = ray.remote(send_op).remote(SERVER_ADDRESS, f"data-{i}", i, i + 1)
+        sent_objs.append(sent_obj)
+        get_obj = recver_proxy_actor.get_data.remote(i, i + 1)
+        get_objs.append(get_obj)
+    for result in ray.get(sent_objs):
+        assert result
+    
+    for i in range(NUM_DATA):
+        assert f"data-{i}" in ray.get(get_objs)
+    ray.shutdown()
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(pytest.main(["-v", __file__]))
