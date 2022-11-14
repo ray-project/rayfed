2eb8db936c2e3f63c3b32ba172ca51e9d1320b7f
Qing Wang <kingchin1218@gmail.com>
2022-11-14 12:14:52 +0800

    Fix CI (#37)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,20 @@
+import multiprocessing
 import pytest
 import fed
 
 
-def test_fed_apis():
+def run():
     cluster = {'alice': '127.0.0.1:11010', 'bob': '127.0.0.1:11011'}
     fed.init(cluster=cluster, party="alice")
     assert fed.get_cluster() == cluster
     assert fed.get_party() == "alice"
     fed.shutdown()
 
+def test_fed_apis():
+    p_alice = multiprocessing.Process(target=run)
+    p_alice.start()
+    p_alice.join()
+    assert p_alice.exitcode == 0
 
 if __name__ == "__main__":
     import sys
