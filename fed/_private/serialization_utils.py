a2d356442949ad81b6774e4678705ddb602a4224
Qing Wang <kingchin1218@gmail.com>
2022-12-05 19:47:01 +0800

    Support set whitelist for pickle when transferring data across parties. (#11)

---
+++ fed/_private/serialization_utils.py
@@ -0,0 +1,62 @@
+import yaml
+import io
+import cloudpickle
+import fed
+import pickle5
+
+
+_pickle_whitelist = None
+
+
+class RestrictedUnpickler(pickle5.Unpickler):
+    def find_class(self, module, name):
+        if _pickle_whitelist is None or (
+            module in _pickle_whitelist
+            and (_pickle_whitelist[module] is None or name in _pickle_whitelist[module])
+        ):
+            return super().find_class(module, name)
+
+        if module == "fed._private": # TODO(qwang): Not sure if it works.
+            return super().find_class(module, name)
+
+        # Forbid everything else.
+        raise pickle5.UnpicklingError("global '%s.%s' is forbidden" % (module, name))
+
+
+def _restricted_loads(
+    serialized_data,
+    *,
+    fix_imports=True,
+    encoding="ASCII",
+    errors="strict",
+    buffers=None,
+):
+    if isinstance(serialized_data, str):
+        raise TypeError("Can't load pickle from unicode string")
+    file = io.BytesIO(serialized_data)
+    return RestrictedUnpickler(
+        file, fix_imports=fix_imports, buffers=buffers, encoding=encoding, errors=errors
+    ).load()
+
+
+def _apply_loads_function_with_whitelist():
+    global _pickle_whitelist
+    whitelist_path = fed._private.constants.RAYFED_PICKLE_WHITELIST_CONFIG_PATH
+    if whitelist_path is None:
+        return
+
+    _pickle_whitelist = yaml.safe_load(open(whitelist_path, "rt")).get(
+        "pickle_whitelist", None
+    )
+    if _pickle_whitelist is None:
+        return
+
+    if "*" in _pickle_whitelist:
+        _pickle_whitelist = None
+    for module, attr_list in _pickle_whitelist.items():
+        if "*" in attr_list:
+            _pickle_whitelist[module] = None
+    cloudpickle.loads = _restricted_loads
+
+
+_apply_loads_function_with_whitelist()
---
+++ tests/serializations_tests/test_unpickle_with_whitelist.py
@@ -0,0 +1,78 @@
+import yaml
+import tempfile
+import os
+import fed
+import multiprocessing
+import numpy
+import pickle5
+import ray
+
+
+@fed.remote
+def generate_wrong_type():
+    class WrongType:
+        pass
+
+    return WrongType()
+
+
+@fed.remote
+def generate_allowed_type():
+    return numpy.array([1, 2, 3, 4, 5])
+
+
+@fed.remote
+def pass_arg(d):
+    return True
+
+
+def run(party):
+    cluster = {'alice': '127.0.0.1:11010', 'bob': '127.0.0.1:11011'}
+    fed.init(cluster=cluster, party=party)
+
+    # Test passing an allowed type.
+    o1 = generate_allowed_type.party("alice").remote()
+    o2 = pass_arg.party("bob").remote(o1)
+    res = fed.get(o2)
+    assert res
+
+    # Test passing an unallowed type.
+    o3 = generate_wrong_type.party("alice").remote()
+    o4 = pass_arg.party("bob").remote(o3)
+    if party == "bob":
+        try:
+            fed.get(o4)
+            assert False, "This code path shouldn't be reached."
+        except Exception as e:
+            assert "_pickle.UnpicklingError" in str(e)
+    else:
+        import time
+        time.sleep(10)
+    fed.shutdown()
+
+
+def test_restricted_loads():
+    config_path = "/tmp/test.yaml"
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        whitelist_config = {
+            "pickle_whitelist": {
+                "numpy.core.numeric": ["*"],
+                "numpy": ["dtype"],
+            }
+        }
+        yaml.safe_dump(whitelist_config, open(config_path, "wt"))
+
+    os.environ["RAYFED_PICKLE_WHITELIST_CONFIG_PATH"] = config_path
+    p_alice = multiprocessing.Process(target=run, args=('alice',))
+    p_bob = multiprocessing.Process(target=run, args=('bob',))
+    p_alice.start()
+    p_bob.start()
+    p_alice.join()
+    p_bob.join()
+    assert p_alice.exitcode == 0 and p_bob.exitcode == 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-sv", __file__]))
