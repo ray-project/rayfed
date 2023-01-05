82d7a56455da0902f0d1c61e73e90e0c0137e61b
Qing Wang <kingchin1218@gmail.com>
2023-01-05 14:22:48 +0800

    Initialize

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# All tests should be ran with TLS enabled for Ray cluster.
+python tool/generate_tls_certs.py
+export RAY_USE_TLS=1
+export RAY_TLS_SERVER_CERT="/tmp/rayfed/test-certs/server.crt"
+export RAY_TLS_SERVER_KEY="/tmp/rayfed/test-certs/server.key"
+export RAY_TLS_CA_CERT="/tmp/rayfed/test-certs/server.crt"
+
+pushd tests
+python3 -m pytest -v -s test_*
+python3 -m pytest -v -s serializations_tests/test_*
+popd
