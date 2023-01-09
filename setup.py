ba827c5345ae52cd38f96e15e4d0aced9de520bb
anakinxc <103552181+anakinxc@users.noreply.github.com>
2023-01-09 21:18:42 +0800

    Make rayfed package platform independent (#25)
    * Make rayfed package platform independent
    
    * bump version

--- setup.py
+++ setup.py
@@ -9,15 +9,7 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Default Linux platform tag
-plat_name = "manylinux2014_x86_64"
-
-if sys.platform == "darwin":
-    # Due to a bug in conda x64 python, platform tag has to be 10_16 for X64 wheel
-    if platform.machine() == "x86_64":
-        plat_name = "macosx_10_16_x86_64"
-    else:
-        plat_name = "macosx_11_0_arm64"
+plat_name = "any"
 
 def read_requirements():
     requirements = []
@@ -47,7 +39,7 @@ class CleanCommand(setuptools.Command):
 
 setup(
     name='secretflow-rayfed',
-    version='0.1.0b0',
+    version='0.1.0b1',
     license='Apache 2.0',
     description='A multiple parties involved execution engine on the top of Ray.',
     long_description=long_description,
