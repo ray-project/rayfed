43af4d78a314bbc11f40341951eb47dd66e99611
Krout0n <me+git@tokinia.me>
2023-02-06 18:34:12 +0900

    Refine the comment

--- fed/_private/grpc_options.py
+++ fed/_private/grpc_options.py
@@ -32,7 +32,7 @@ _GRPC_MAX_RECEIVE_MESSAGE_LENGTH = _DEFAULT_GRPC_MAX_RECEIVE_MESSAGE_LENGTH
 
 def set_max_message_length(new_max_size):
     """
-    Set the maximum length of gRPC messages.
+    Set the maximum length in bytes of gRPC messages.
     NOTE: The default maximum length is 500MB(500 * 1024 * 1024)
     """
     global _GRPC_MAX_SEND_MESSAGE_LENGTH
