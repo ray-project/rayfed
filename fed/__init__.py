26f8bca4077c1f8311ce4660cd63a6c184dea7c6
Krout0n <me+git@tokinia.me>
2023-02-06 18:47:04 +0900

    Refactor: Remove expose of set_max_message_length

--- fed/__init__.py
+++ fed/__init__.py
@@ -16,7 +16,6 @@ from fed.api import (get, get_cluster, get_party, get_tls, init, kill, remote,
                      shutdown)
 from fed.barriers import recv, send
 from fed.fed_object import FedObject
-from fed._private.grpc_options import set_max_message_length
 
 __all__ = [
     "get",
@@ -29,6 +28,5 @@ __all__ = [
     "shutdown",
     "recv",
     "send",
-    "set_max_message_length",
     "FedObject",
 ]
