11413f7599e3ea7426addeaa3811002080ebbd93
Krout0n <me+git@tokinia.me>
2023-02-11 13:01:17 +0900

    WIP: RecursionError: maximum recursion depth exceeded in comparison

--- tests/test_set_grpc_max_size_in_bytes.py
+++ tests/test_set_grpc_max_size_in_bytes.py
@@ -16,22 +16,42 @@ import multiprocessing
 import pytest
 import fed
 
-def run():
+@fed.remote
+class MyActor:
+    def __init__(self, party, value):
+        self._party = party
+        self.value = value
+
+@fed.remote
+def concat_str(val1, val2):
+    return f"{val1} {val2}"
+
+def run(party):
     cluster = {
         'alice': {'address': '127.0.0.1:11010'},
         'bob': {'address': '127.0.0.1:11011'},
     }
-    fed.init(address='local', cluster=cluster, party="alice", grpc_max_size_in_bytes=100)
-    too_large_objects = [x for x in range(1000)] 
+    fed.init(address='local', cluster=cluster, party=party, cross_silo_messages_max_size_in_bytes=10)
 
+    # The given strings are too long for cross_silo_messages_max_size_in_bytes
+    val_alice = MyActor.party("alice").remote("alice", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+    val_bob = MyActor.party("bob").remote("bob", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+    concatenated_val_obj = concat_str.party("bob").remote(val_alice, val_bob)
+    result = fed.get(concatenated_val_obj)
+
+    assert len(result) == 445
 
     fed.shutdown()
 
-def test_fed_apis():
-    p_alice = multiprocessing.Process(target=run)
+def test_grpc_max_size():
+    p_alice = multiprocessing.Process(target=run, args=('alice',))
+    p_bob = multiprocessing.Process(target=run, args=('bob',))
     p_alice.start()
+    p_bob.start()
     p_alice.join()
-    assert p_alice.exitcode == 0
+    p_bob.join()
+    assert p_alice.exitcode == 0 and p_bob.exitcode == 0
 
 
 if __name__ == "__main__":
