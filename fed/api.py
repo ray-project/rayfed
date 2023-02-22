324b8e0bae71316a9c6d428b3e5deaef3e06a8d1
albert.zah <albert.zah@antgroup.com>
2023-02-22 10:37:53 +0800

    complete docstring.

--- fed/api.py
+++ fed/api.py
@@ -107,6 +107,9 @@ def init(
                     "cert": "bob's server cert",
                     "key": "bob's server cert key",
                 }
+            
+            There is some known problems when there are more than two parties
+            and every party has different root ca, and we're working on it.
         logging_level: optional; the logging level, could be `debug`, `info`,
             `warning`, `error`, `critical`, not case sensititive.
         cross_silo_grpc_retry_policy: a dict descibes the retry policy for
