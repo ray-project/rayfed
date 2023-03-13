56c5258c31d63f96a276aedecd8f778485db8764
Qing Wang <kingchin1218@gmail.com>
2023-03-13 17:07:01 +0800

    Fix lint
    Signed-off-by: Qing Wang <kingchin1218@gmail.com>

--- tests/test_transport_proxy_tls.py
+++ tests/test_transport_proxy_tls.py
@@ -21,7 +21,6 @@ import fed._private.compatible_utils as compatible_utils
 
 from fed.barriers import RecverProxyActor, send, start_send_proxy
 from fed.cleanup import wait_sending
-from fed._private.constants import KEY_OF_TLS_CONFIG
 
 from fed._private.constants import (
     KEY_OF_CLUSTER_CONFIG,
