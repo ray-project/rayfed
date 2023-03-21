54cfc86288538d45afa9e623de56e3df5a51ea07
albert.zah <albert.zah@antgroup.com>
2023-03-21 10:35:06 +0800

    Bump version.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ class CleanCommand(setuptools.Command):
 
 setup(
     name='rayfed',
-    version='0.1.1a2',
+    version='0.1.1a3',
     license='Apache 2.0',
     description='A multiple parties joint, distributed execution engine based on Ray,'
                 'to help build your own federated learning frameworks in minutes.',
