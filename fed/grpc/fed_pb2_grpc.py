ed7c7d28bea11be1a578bd52bafdd94fdbbe34bd
Qing Wang <kingchin1218@gmail.com>
2023-04-10 14:52:13 +0800

    Fix building

--- fed/grpc/fed_pb2_grpc.py
+++ fed/grpc/fed_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import fed_pb2 as fed__pb2
+import fed.grpc.fed_pb2 as fed__pb2
 
 
 class GrpcServiceStub(object):
