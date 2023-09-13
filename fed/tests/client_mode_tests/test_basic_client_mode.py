7a3b37efdd539ab78da105455ce66edc90edd5c5
Qing Wang <kingchin1218@gmail.com>
2023-09-13 17:26:40 +0800

    1

--- fed/tests/client_mode_tests/test_basic_client_mode.py
+++ fed/tests/client_mode_tests/test_basic_client_mode.py
@@ -18,7 +18,7 @@ import pytest
 import ray
 import fed
 import fed._private.compatible_utils as compatible_utils
-import fed.tests.test_utils as fed_test_utils
+from fed.tests.test_utils import ray_client_mode_setup
 
 
 @fed.remote
@@ -83,8 +83,8 @@ def run(party):
     ray.shutdown()
 
 
-@pytest.fixture(fed_test_utils.ray_client_mode_setup)
-def test_fed_get_in_2_parties():
+
+def test_fed_get_in_2_parties(ray_client_mode_setup):
 
     p_alice = multiprocessing.Process(target=run, args=('alice',))
     p_bob = multiprocessing.Process(target=run, args=('bob',))
