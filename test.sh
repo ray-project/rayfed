2f7401a0f4c9a4ea9ca81a38ed9d0efc4d7e297c
albert.zah <albert.zah@antgroup.com>
2024-01-12 06:40:58 +0000

    Fix test.sh

--- test.sh
+++ test.sh
@@ -13,6 +13,6 @@ export RAY_TLS_CA_CERT="/tmp/rayfed/test-certs/server.crt"
 directory="fed/tests"
 command="pytest -vs"
  
-find "$directory" -type f -exec bash -c "eval '$command'" \;
+find "$directory" -type f -mindepth 1 -name "test_*" -exec bash -c "eval '$command'" \;
 
 echo "All tests finished."
